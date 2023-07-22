f7ae4c5a2be7f76a205a2655af4ffa9ab86b12ba
十七 <10858187+shiqi547git@user.noreply.gitee.com>
2023-07-23 01:05:45 +0800

    feature: 增加快慢指针法

--- 链表/6. 判断链表中是否有环.js
+++ 链表/6. 判断链表中是否有环.js
@@ -37,6 +37,16 @@ function hasCycle( head ) {
     }
     return false
 }
+
+function hasCycle( head ) {
+    let fast = slow = head
+    while(fast && fast.next) {
+        fast = fast.next.next
+        slow = slow.next
+        if(fast === slow) return true
+    }
+    return false
+}
 module.exports = {
     hasCycle : hasCycle
 };
