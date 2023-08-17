7adc140bb1b3dd5f0723d33a7c62a65d9ea096f5
wushiqing <wsq1375868809@163.com>
2023-08-17 10:56:21 +0800

    test: 测试webhook

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-
-function change(obj , strArr = []){
-    if(!(obj instanceof Object))return obj
-    for(let key in obj){
-        if(obj.hasOwnProperty(key)){
-            if(obj[key] instanceof Object){
-                change(obj[key], strArr)
-            }
-        }
+function change(obj, strArr = []) {
+  if (!(obj instanceof Object)) return obj;
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (obj[key] instanceof Object) {
+        change(obj[key], strArr);
+      }
     }
+  }
 }
 
 let array = [5, 3, 8, 4, 6];
 
-for(let i = 0; i < array.length-1; i++) {
-  for(let j = i+1; j < array.length ; j++) {
-    if(array[i] > array[j]) {
+for (let i = 0; i < array.length - 1; i++) {
+  for (let j = i + 1; j < array.length; j++) {
+    if (array[i] > array[j]) {
       // 交换元素
       let temp = array[i];
       array[i] = array[j];
@@ -23,9 +22,7 @@ for(let i = 0; i < array.length-1; i++) {
   }
 }
 
-console.log(array);  // 输出: [3, 
-
-
+console.log(array); // 输出: [3,
 
 console.log(JSON.stringify(112n));
 // console.log( "1",JSON.stringify(122222222222222222222n));
