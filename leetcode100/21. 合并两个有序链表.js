9a7c93bea0d31b72fb19b15f8b72c4b2c82978c5
十七 <10858187+shiqi547git@user.noreply.gitee.com>
2023-08-17 21:01:26 +0800

    feature: 合并两个有序链表

---
+++ leetcode100/21. 合并两个有序链表.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let temp = new ListNode(0)
+    const head = temp
+    while(list1 && list2) {
+        if(list1.val < list2.val) {
+            temp = temp.next = list1
+            list1 = list1.next
+        }else {
+            temp = temp.next = list2
+            list2 = list2.next
+        }
+    }
+    temp.next = list1 === null ? list2 : list1
+    return head.next
+};
