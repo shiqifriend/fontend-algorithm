e2b4ef6893311657e3546a446e606d9a981e293d
wushiqing <wsq1375868809@163.com>
2023-08-17 18:49:25 +0800

    feat: 删除链表的倒数第n个节点

---
+++ leetcode100/19. 删除链表的倒数第n个节点.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  if (!head) return head;
+  let slow = (fast = head);
+  while (n--) {
+    fast = fast.next;
+  }
+  if (!fast) return head.next;
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return head;
+};
