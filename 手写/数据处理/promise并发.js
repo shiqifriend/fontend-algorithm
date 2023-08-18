ae344d7f6c698f3f89580ac1930ebeb392c2bb21
wushiqing <wsq1375868809@163.com>
2023-08-18 14:16:45 +0800

    feature: promise并发

---
+++ 手写/数据处理/promise并发.js
@@ -0,0 +1,40 @@
+const limitFn = (limit) => {
+  const queue = [];
+  let activeCount = 0;
+
+  const next = () => {
+    activeCount--;
+
+    if (queue.length > 0) {
+      queue.shift()();
+    }
+  };
+
+  const run = async (fn, resolve, ...args) => {
+    activeCount++;
+
+    const result = (async () => fn(...args))();
+
+    try {
+      const res = await result;
+      resolve(res);
+    } catch {}
+
+    next();
+  };
+
+  const enqueue = (fn, resolve, ...args) => {
+    queue.push(run.bind(null, fn, resolve, ...args));
+
+    if (activeCount < limit && queue.length > 0) {
+      queue.shift()();
+    }
+  };
+
+  const generator = (fn, ...args) =>
+    new Promise((resolve) => {
+      enqueue(fn, resolve, ...args);
+    });
+
+  return generator;
+};
